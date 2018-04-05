f2f74f659a48e327b2bd4c3921d1c953a9ccdfca
ogasada <ogasada0@gmail.com>
2018-04-06 08:41:11 +0900

    test: add resultMap tag convert test to SqlMapFileConvertetTest

--- src/test/kotlin/jp/ogasada/ibatistomybatis3/converter/SqlMapFileConverterTest.kt
+++ src/test/kotlin/jp/ogasada/ibatistomybatis3/converter/SqlMapFileConverterTest.kt
@@ -13,6 +13,13 @@ internal class SqlMapFileConverterTest {
         assertEquals(1, sqlMapTagsBeforeConvert.length)
         val mapperTagsBeforeConvert = loadedDocument.getElementsByTagName("mapper")
         assertEquals(0, mapperTagsBeforeConvert.length)
+        assertTrue(existsAttribute(loadedDocument, "resultMap", "class"))
+        assertTrue(existsAttribute(loadedDocument, "resultMap", "id"))
+        assertTrue(existsAttribute(loadedDocument, "resultMap", "groupBy"))
+        assertFalse(existsAttribute(loadedDocument, "resultMap", "type"))
+        assertEquals(attributeValue(loadedDocument, "resultMap", "id"), "findResult")
+        assertEquals(attributeValue(loadedDocument, "resultMap", "class"), "HashMap")
+        assertEquals(attributeValue(loadedDocument, "resultMap", "groupBy"), "id")
 
         val convertedDocument: Document = SqlMapFileConverter.convert(loadedDocument)
 
@@ -20,6 +27,12 @@ internal class SqlMapFileConverterTest {
         assertEquals(0, sqlMapTagsAfterConvert.length)
         val mapperTagsAfterConvert = convertedDocument.getElementsByTagName("mapper")
         assertEquals(1, mapperTagsAfterConvert.length)
+        assertFalse(existsAttribute(loadedDocument, "resultMap", "class"))
+        assertTrue(existsAttribute(loadedDocument, "resultMap", "id"))
+        assertFalse(existsAttribute(loadedDocument, "resultMap", "groupBy"))
+        assertTrue(existsAttribute(loadedDocument, "resultMap", "type"))
+        assertEquals(attributeValue(loadedDocument, "resultMap", "id"), "findResult")
+        assertEquals(attributeValue(loadedDocument, "resultMap", "type"), "HashMap")
     }
 
     private fun loadValidDocument(): Document {
@@ -29,7 +42,7 @@ internal class SqlMapFileConverterTest {
                 "  \"http://ibatis.apache.org/dtd/sql-map-2.dtd\">\n" +
                 "\n" +
                 "<sqlMap namespace=\"jp.ogasada.test\">\n" +
-                "  <resultMap id=\"findResult\" class=\"HashMap\">\n" +
+                "  <resultMap id=\"findResult\" class=\"HashMap\" groupBy=\"id\">\n" +
                 "    <result property=\"id\" column=\"id\" javaType=\"int\" />\n" +
                 "    <result property=\"name\" column=\"name\" javaType=\"String\" />\n" +
                 "  </resultMap>\n" +
