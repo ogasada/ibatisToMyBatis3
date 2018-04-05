b3d904da996b40d5652ae96c5a92ae8ff2589c28
ogasada <ogasada0@gmail.com>
2018-04-06 08:48:59 +0900

    docs: add example of convert

--- src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/ResultMapTagConverter.kt
+++ src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/ResultMapTagConverter.kt
@@ -3,6 +3,24 @@ package jp.ogasada.ibatistomybatis3.converter
 import org.w3c.dom.Document
 
 object ResultMapTagConverter: ITagConverter {
+
+    /**
+     * convert `resultMap` tag as follows
+     *
+     * before
+     *
+     * ```
+     * <resultMap id="findResult" class="HashMap" groupBy="id">
+     * </resultMap>
+     * ```
+     *
+     * after
+     *
+     * ```
+     * <resultMap id="findResult" type="HashMap">
+     * </resultMap>
+     * ```
+     */
     override fun convert(xmlDocument: Document): Document = xmlDocument
             .convertAttributeName("resultMap", "class", "type")
             .removeAttribute("resultMap", "groupBy")
--- src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/SqlMapTagConverter.kt
+++ src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/SqlMapTagConverter.kt
@@ -3,5 +3,23 @@ package jp.ogasada.ibatistomybatis3.converter
 import org.w3c.dom.Document
 
 object SqlMapTagConverter: ITagConverter {
+
+    /**
+     * convert `sqlMap` tag as follows
+     *
+     * before
+     *
+     * ```
+     * <sqlMap namespace="jp.ogasada.test">
+     * </sqlMap>
+     * ```
+     *
+     * after
+     *
+     * ```
+     * <mapper namespace="jp.ogasada.test">
+     * </mapper>
+     * ```
+     */
     override fun convert(xmlDocument: Document): Document = xmlDocument.convertTagName("sqlMap", "mapper")
 }
