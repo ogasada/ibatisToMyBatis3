028529c4f86e769a4d74d760a144cbfbeb1da6b8
ogasada <ogasada0@gmail.com>
2018-04-05 10:31:44 +0900

    test: add resultMap tag convert test

---
+++ src/test/kotlin/jp/ogasada/ibatistomybatis3/converter/ResultMapTagConverterTest.kt
@@ -0,0 +1,59 @@
+package jp.ogasada.ibatistomybatis3.converter
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+
+internal class ResultMapTagConverterTest {
+    @Test
+    fun convertForValidDocument() {
+        val loadedDocument = loadValidDocument()
+
+        val resultMapTagsBeforeConvert = loadedDocument.getElementsByTagName("resultMap")
+        assertEquals(1, resultMapTagsBeforeConvert.length)
+        assertTrue(existsAttribute(loadedDocument, "resultMap", "class"))
+        assertTrue(existsAttribute(loadedDocument, "resultMap", "id"))
+        assertTrue(existsAttribute(loadedDocument, "resultMap", "groupBy"))
+        assertFalse(existsAttribute(loadedDocument, "resultMap", "type"))
+        assertEquals(attributeValue(loadedDocument, "resultMap", "id"), "findResult")
+        assertEquals(attributeValue(loadedDocument, "resultMap", "class"), "HashMap")
+        assertEquals(attributeValue(loadedDocument, "resultMap", "groupBy"), "id")
+
+        val convertedDocument = ResultMapTagConverter.convert(loadedDocument)
+
+        val resultMapTagsAfterConvert = convertedDocument.getElementsByTagName("resultMap")
+        assertEquals(1, resultMapTagsAfterConvert.length)
+        assertFalse(existsAttribute(loadedDocument, "resultMap", "class"))
+        assertTrue(existsAttribute(loadedDocument, "resultMap", "id"))
+        assertFalse(existsAttribute(loadedDocument, "resultMap", "groupBy"))
+        assertTrue(existsAttribute(loadedDocument, "resultMap", "type"))
+        assertEquals(attributeValue(loadedDocument, "resultMap", "id"), "findResult")
+        assertEquals(attributeValue(loadedDocument, "resultMap", "type"), "HashMap")
+    }
+
+    private fun loadValidDocument(): Document {
+        val xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<!DOCTYPE sqlMap\n" +
+                "  PUBLIC \"-//ibatis.apache.org//DTD SQL Map 2.0//EN\"\n" +
+                "  \"http://ibatis.apache.org/dtd/sql-map-2.dtd\">\n" +
+                "\n" +
+                "<sqlMap namespace=\"jp.ogasada.test\">\n" +
+                "  <resultMap id=\"findResult\" class=\"HashMap\" groupBy=\"id\">\n" +
+                "    <result property=\"id\" column=\"id\" javaType=\"int\" />\n" +
+                "    <result property=\"name\" column=\"name\" javaType=\"String\" />\n" +
+                "  </resultMap>\n" +
+                "  <select id=\"find\" resultMap=\"findResult\" parameterClass=\"long\">\n" +
+                "    SELECT\n" +
+                "      id,\n" +
+                "      name\n" +
+                "    FROM\n" +
+                "      testTable\n" +
+                "    WHERE\n" +
+                "      id = #id#\n" +
+                "  </select>\n" +
+                "</sqlMap>\n"
+
+        return loadXML(xml)
+    }
+}
--- src/test/kotlin/jp/ogasada/ibatistomybatis3/converter/XmlDocumentUtil.kt
+++ src/test/kotlin/jp/ogasada/ibatistomybatis3/converter/XmlDocumentUtil.kt
@@ -1,6 +1,7 @@
 package jp.ogasada.ibatistomybatis3.converter
 
 import org.w3c.dom.Document
+import org.w3c.dom.Element
 import org.xml.sax.InputSource
 import java.io.StringReader
 import javax.xml.parsers.DocumentBuilderFactory
@@ -13,3 +14,17 @@ internal fun loadXML(xml: String): Document {
     inputSource.characterStream = StringReader(xml)
     return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(inputSource)!!
 }
+
+/**
+ * return true when exists [attributeName] in [tagName] of [xmlDocument]
+ */
+internal fun existsAttribute(xmlDocument: Document, tagName: String, attributeName: String): Boolean {
+    val tags = xmlDocument.getElementsByTagName(tagName)
+    return (0 until tags.length).any {
+        (tags.item(it) as Element).hasAttribute(attributeName)
+    }
+}
+
+internal fun attributeValue(xmlDocument: Document, tagName: String, attributeName: String, index: Int = 0): String =
+        (xmlDocument.getElementsByTagName(tagName).item(index) as Element).getAttribute(attributeName)
+
