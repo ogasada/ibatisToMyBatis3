a4456d12eb50c8f5dc3094899ad07462deef35c7
ogasada <ogasada0@gmail.com>
2018-04-12 09:18:38 +0900

    refactor: change to test function by tags

--- src/test/kotlin/jp/ogasada/ibatistomybatis3/converter/SqlMapFileConverterTest.kt
+++ src/test/kotlin/jp/ogasada/ibatistomybatis3/converter/SqlMapFileConverterTest.kt
@@ -9,10 +9,32 @@ internal class SqlMapFileConverterTest {
     fun convert() {
         val loadedDocument = loadValidDocument()
 
+        sqlMapTagTestBeforeConvert(loadedDocument)
+        resultMapTagTestBeforeConvert(loadedDocument)
+        resultTagTestBeforeConvert(loadedDocument)
+
+        val convertedDocument: Document = SqlMapFileConverter.convert(loadedDocument)
+
+        sqlMapTagTestAfterConvert(convertedDocument)
+        resultMapTagTestAfterConvert(convertedDocument)
+        resultTagTestAfterConvert(convertedDocument)
+    }
+
+    private fun sqlMapTagTestBeforeConvert(loadedDocument: Document) {
         val sqlMapTagsBeforeConvert = loadedDocument.getElementsByTagName("sqlMap")
         assertEquals(1, sqlMapTagsBeforeConvert.length)
         val mapperTagsBeforeConvert = loadedDocument.getElementsByTagName("mapper")
         assertEquals(0, mapperTagsBeforeConvert.length)
+    }
+
+    private fun sqlMapTagTestAfterConvert(convertedDocument: Document) {
+        val sqlMapTagsAfterConvert = convertedDocument.getElementsByTagName("sqlMap")
+        assertEquals(0, sqlMapTagsAfterConvert.length)
+        val mapperTagsAfterConvert = convertedDocument.getElementsByTagName("mapper")
+        assertEquals(1, mapperTagsAfterConvert.length)
+    }
+
+    private fun resultMapTagTestBeforeConvert(loadedDocument: Document) {
         assertTrue(existsAttribute(loadedDocument, "resultMap", "class"))
         assertTrue(existsAttribute(loadedDocument, "resultMap", "id"))
         assertTrue(existsAttribute(loadedDocument, "resultMap", "groupBy"))
@@ -20,6 +42,18 @@ internal class SqlMapFileConverterTest {
         assertEquals(attributeValue(loadedDocument, "resultMap", "id"), "findResult")
         assertEquals(attributeValue(loadedDocument, "resultMap", "class"), "HashMap")
         assertEquals(attributeValue(loadedDocument, "resultMap", "groupBy"), "id")
+    }
+
+    private fun resultMapTagTestAfterConvert(convertedDocument: Document) {
+        assertFalse(existsAttribute(convertedDocument, "resultMap", "class"))
+        assertTrue(existsAttribute(convertedDocument, "resultMap", "id"))
+        assertFalse(existsAttribute(convertedDocument, "resultMap", "groupBy"))
+        assertTrue(existsAttribute(convertedDocument, "resultMap", "type"))
+        assertEquals(attributeValue(convertedDocument, "resultMap", "id"), "findResult")
+        assertEquals(attributeValue(convertedDocument, "resultMap", "type"), "HashMap")
+    }
+
+    private fun resultTagTestBeforeConvert(loadedDocument: Document) {
         assertEquals(attributeValue(loadedDocument, "result", "property", 0), "id")
         assertEquals(attributeValue(loadedDocument, "result", "property", 1), "name")
         assertEquals(attributeValue(loadedDocument, "result", "property", 2), "detailList")
@@ -32,31 +66,21 @@ internal class SqlMapFileConverterTest {
         assertFalse(existsAttribute(loadedDocument, "result", "resultMap", 1))
         assertTrue(existsAttribute(loadedDocument, "result", "resultMap", 2))
         assertEquals(attributeValue(loadedDocument, "result", "resultMap", 2), "jp.ogasada.ibatistomybatis3.detailResult")
+    }
 
-        val convertedDocument: Document = SqlMapFileConverter.convert(loadedDocument)
-
-        val sqlMapTagsAfterConvert = convertedDocument.getElementsByTagName("sqlMap")
-        assertEquals(0, sqlMapTagsAfterConvert.length)
-        val mapperTagsAfterConvert = convertedDocument.getElementsByTagName("mapper")
-        assertEquals(1, mapperTagsAfterConvert.length)
-        assertFalse(existsAttribute(loadedDocument, "resultMap", "class"))
-        assertTrue(existsAttribute(loadedDocument, "resultMap", "id"))
-        assertFalse(existsAttribute(loadedDocument, "resultMap", "groupBy"))
-        assertTrue(existsAttribute(loadedDocument, "resultMap", "type"))
-        assertEquals(attributeValue(loadedDocument, "resultMap", "id"), "findResult")
-        assertEquals(attributeValue(loadedDocument, "resultMap", "type"), "HashMap")
-        assertEquals(attributeValue(loadedDocument, "result", "property", 0), "id")
-        assertEquals(attributeValue(loadedDocument, "result", "property", 1), "name")
-        assertEquals(attributeValue(loadedDocument, "result", "property", 2), "detailList")
-        assertEquals(attributeValue(loadedDocument, "result", "column", 0), "id")
-        assertEquals(attributeValue(loadedDocument, "result", "column", 1), "name")
-        assertEquals(attributeValue(loadedDocument, "result", "javaType", 0), "int")
-        assertEquals(attributeValue(loadedDocument, "result", "javaType", 1), "String")
-        assertEquals(attributeValue(loadedDocument, "result", "javaType", 2), "List")
-        assertFalse(existsAttribute(loadedDocument, "result", "resultMap", 0))
-        assertFalse(existsAttribute(loadedDocument, "result", "resultMap", 1))
-        assertTrue(existsAttribute(loadedDocument, "result", "resultMap", 2))
-        assertEquals(attributeValue(loadedDocument, "result", "resultMap", 2), "detailResult")
+    private fun resultTagTestAfterConvert(convertedDocument: Document) {
+        assertEquals(attributeValue(convertedDocument, "result", "property", 0), "id")
+        assertEquals(attributeValue(convertedDocument, "result", "property", 1), "name")
+        assertEquals(attributeValue(convertedDocument, "result", "property", 2), "detailList")
+        assertEquals(attributeValue(convertedDocument, "result", "column", 0), "id")
+        assertEquals(attributeValue(convertedDocument, "result", "column", 1), "name")
+        assertEquals(attributeValue(convertedDocument, "result", "javaType", 0), "int")
+        assertEquals(attributeValue(convertedDocument, "result", "javaType", 1), "String")
+        assertEquals(attributeValue(convertedDocument, "result", "javaType", 2), "List")
+        assertFalse(existsAttribute(convertedDocument, "result", "resultMap", 0))
+        assertFalse(existsAttribute(convertedDocument, "result", "resultMap", 1))
+        assertTrue(existsAttribute(convertedDocument, "result", "resultMap", 2))
+        assertEquals(attributeValue(convertedDocument, "result", "resultMap", 2), "detailResult")
     }
 
     private fun loadValidDocument(): Document {
