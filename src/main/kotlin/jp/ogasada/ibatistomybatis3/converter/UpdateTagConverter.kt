4bf40fec29172fd2482c35f50c0c41df5c70fac7
ogasada <ogasada0@gmail.com>
2018-04-16 18:41:36 +0900

    feat: add update tag converter

---
+++ src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/UpdateTagConverter.kt
@@ -0,0 +1,26 @@
+package jp.ogasada.ibatistomybatis3.converter
+
+import org.w3c.dom.Document
+
+object UpdateTagConverter: ITagConverter {
+
+    /**
+     * convert `update` tag as follows
+     *
+     * before
+     *
+     * ```
+     * <update id="insert" parameterClass="jp.ogasada.ibatistomybatis3.TestTableEntity" >
+     * ```
+     *
+     * after
+     *
+     * ```
+     * <update id="insert" parameterType="jp.ogasada.ibatistomybatis3.TestTableEntity" >
+     * ```
+     */
+    override fun convert(xmlDocument: Document): Document = xmlDocument
+            .convertAttributeName("update", "parameterClass", "parameterType")
+}
+
+
