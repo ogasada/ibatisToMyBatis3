95b12276c07d01a6d6467c3b56b99ff403bb9fc2
ogasada <ogasada0@gmail.com>
2018-04-17 17:29:33 +0900

    feat: add isEqual tag converter

---
+++ src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/IsEqualTagConverter.kt
@@ -0,0 +1,53 @@
+package jp.ogasada.ibatistomybatis3.converter
+
+import org.w3c.dom.Document
+
+object IsEqualTagConverter: ITagConverter {
+
+    /**
+     * convert `isEqual` tag as follows
+     *
+     * before
+     *
+     * ```
+     * <isEqual open="and (" property="name" compareValue="foo" close=")">
+     *   name = #name#
+     *     <isEqual prepend="or" property="name2" compareProperty="compareName2">
+     *       name = #name2#
+     *     </isEqual>
+     * </isEqual>
+     * ```
+     *
+     * after
+     *
+     * ```
+     * <if test="name.toString().equals('foo'.toString()))">
+     *   and ( name = #name#
+     *     <if test="name2.toString().equals(compareName2.toString()))">
+     *       or name = #name2#
+     *     </if> )
+     * </if>
+     * ```
+     */
+    override fun convert(xmlDocument: Document): Document = xmlDocument
+            .prependAttributeValueToTextContent("isEqual", "prepend")
+            .prependAttributeValueToTextContent("isEqual", "open")
+            .appendAttributeValueToTextContent("isEqual", "close")
+            .createNewAttribute("isEqual", "test") { node ->
+                val attributeValue = node.getAttribute("property")
+                when {
+                    node.hasAttribute("compareValue") -> "$attributeValue.toString().equals('${node.getAttribute("compareValue")}'.toString()))"
+                    node.hasAttribute("compareProperty") -> "$attributeValue.toString().equals(${node.getAttribute("compareProperty")}.toString()))"
+                    else -> ""
+                }
+            }
+            .removeAttribute("isEqual", "prepend")
+            .removeAttribute("isEqual", "open")
+            .removeAttribute("isEqual", "close")
+            .removeAttribute("isEqual", "property")
+            .removeAttribute("isEqual", "compareValue")
+            .removeAttribute("isEqual", "compareProperty")
+            .convertTagName("isEqual", "if")
+}
+
+
