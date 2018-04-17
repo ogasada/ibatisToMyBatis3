ed70b48e6d0260188cc52b03f5ead8384f969265
ogasada <ogasada0@gmail.com>
2018-04-17 16:46:26 +0900

    feat: add isEmpty tag converter

--- src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/ITagConverter.kt
+++ src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/ITagConverter.kt
@@ -2,6 +2,8 @@ package jp.ogasada.ibatistomybatis3.converter
 
 import org.w3c.dom.Document
 import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
 
 interface ITagConverter {
 
@@ -62,5 +64,109 @@ interface ITagConverter {
             }
         }
         return this
+    }
+
+    /**
+     * create new [attribute] using [valueConverter] in [tag] of [this] document
+     */
+    fun Document.createNewAttribute(tag: String, attribute: String, valueConverter: (Element) -> String): Document {
+        val tags = this.getElementsByTagName(tag)
+        (0 until tags.length).forEach {
+            val node = tags.item(it) as Element
+            node.setAttribute(attribute, valueConverter(node))
+        }
+        return this
+    }
+
+    /**
+     * prepend value of [attribute] to text content node in [tag] of [this] document
+     */
+    fun Document.prependAttributeValueToTextContent(tag: String, attribute: String): Document =
+            this.convertAttributeValueToTextContent(tag, attribute) { nodelist, attributeValue ->
+                val firstTextContent = nodelist.findFirstTextContent()
+                if (firstTextContent != null) {
+                    firstTextContent.textContent = firstTextContent.textContent.prepend(attributeValue)
+                }
+            }
+
+    /**
+     * append value of [attribute] to text content node in [tag] of [this] document
+     */
+    fun Document.appendAttributeValueToTextContent(tag: String, attribute: String): Document =
+            this.convertAttributeValueToTextContent(tag, attribute) { nodelist, attributeValue ->
+                val lastTextContent = nodelist.findLastTextContent()
+                if (lastTextContent != null) {
+                    lastTextContent.textContent = lastTextContent.textContent.append(attributeValue)
+                }
+            }
+
+    /**
+     * convert text content using [attributeValueConverter] in [tag] of [this] document
+     */
+    private fun Document.convertAttributeValueToTextContent(tag: String, attribute: String, attributeValueConverter: (nodelist: NodeList, attributeValue: String) -> (Unit)): Document {
+        val tags = this.getElementsByTagName(tag)
+        (0 until tags.length).forEach {
+            val node = tags.item(it) as Element
+            if (node.hasAttribute(attribute)) {
+                attributeValueConverter(node.childNodes, node.getAttribute(attribute))
+            }
+        }
+        return this
+    }
+
+    /**
+     * find first text content in [NodeList]
+     *
+     * return null when not found text content
+     */
+    fun NodeList.findFirstTextContent(): Node? {
+        (0 until this.length).forEach {
+            when (this.item(it).nodeType) {
+                Node.TEXT_NODE -> {
+                    return this.item(it)
+                }
+            }
+        }
+        return null
+    }
+
+    /**
+     * find last text content in [NodeList]
+     *
+     * return null when not found text content
+     */
+    fun NodeList.findLastTextContent(): Node? {
+        (0 until this.length).reversed().forEach {
+            when (this.item(it).nodeType) {
+                Node.TEXT_NODE -> {
+                    return this.item(it)
+                }
+            }
+        }
+        return null
+    }
+
+    /**
+     * prepend [prependText]
+     *
+     * however, append to [prependText] after first blank sequence when [this] string starts with blank
+     */
+    fun String.prepend(prependText: String): String {
+        val pattern = Regex("\\s")
+        val blankString = this.takeWhile { pattern.matches(it.toString()) }
+        val nonBlankString = this.dropWhile { pattern.matches(it.toString()) }
+        return "$blankString $prependText $nonBlankString"
+    }
+
+    /**
+     * append [appendText]
+     *
+     * however, append to [prependText] before first blank sequence when [this] string ends with blank
+     */
+    fun String.append(appendText: String): String {
+        val pattern = Regex("\\s")
+        val blankString = this.takeLastWhile { pattern.matches(it.toString()) }
+        val nonBlankString = this.dropLastWhile { pattern.matches(it.toString()) }
+        return "$nonBlankString $appendText $blankString"
     }
 }
---
+++ src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/IsEmptyTagConverter.kt
@@ -0,0 +1,47 @@
+package jp.ogasada.ibatistomybatis3.converter
+
+import org.w3c.dom.Document
+
+object IsEmptyTagConverter: ITagConverter {
+
+    /**
+     * convert `isEmpty` tag as follows
+     *
+     * before
+     *
+     * ```
+     * <isEmpty open="and (" property="name" close=")">
+     *   name = #name#
+     *     <isEmpty prepend="or" property="name2">
+     *       name = #name2#
+     *     </isEmpty>
+     * </isEmpty>
+     * ```
+     *
+     * after
+     *
+     * ```
+     * <if test="name == null or name == ''">
+     *   and ( name = #name#
+     *     <if test="name2 == null or name2 == ''">
+     *       or name = #name2#
+     *     </if> )
+     * </if>
+     * ```
+     */
+    override fun convert(xmlDocument: Document): Document = xmlDocument
+            .prependAttributeValueToTextContent("isEmpty", "prepend")
+            .prependAttributeValueToTextContent("isEmpty", "open")
+            .appendAttributeValueToTextContent("isEmpty", "close")
+            .createNewAttribute("isEmpty", "test") { node ->
+                val attributeValue = node.getAttribute("property")
+                "$attributeValue == null or $attributeValue == ''"
+            }
+            .removeAttribute("isEmpty", "prepend")
+            .removeAttribute("isEmpty", "open")
+            .removeAttribute("isEmpty", "close")
+            .removeAttribute("isEmpty", "property")
+            .convertTagName("isEmpty", "if")
+}
+
+
