df98425d3b6485df6fc0c243a840520b7d1232ba
ogasada <ogasada0@gmail.com>
2018-04-20 11:55:33 +0900

    test: change test for bug fixed

--- src/test/kotlin/jp/ogasada/ibatistomybatis3/converter/IsLessEqualTagConverterTest.kt
+++ src/test/kotlin/jp/ogasada/ibatistomybatis3/converter/IsLessEqualTagConverterTest.kt
@@ -11,12 +11,12 @@ internal class IsLessEqualTagConverterTest {
 
         val isLessEqualTagsBeforeConvert = loadedDocument.getElementsByTagName("isLessEqual")
         assertEquals(2, isLessEqualTagsBeforeConvert.length)
-        assertEquals("and (", attributeValue(loadedDocument, "isLessEqual", "open", 0))
+        assertEquals("and", attributeValue(loadedDocument, "isLessEqual", "prepend", 0))
+        assertEquals("(", attributeValue(loadedDocument, "isLessEqual", "open", 0))
         assertEquals(")", attributeValue(loadedDocument, "isLessEqual", "close", 0))
         assertEquals("id", attributeValue(loadedDocument, "isLessEqual", "property", 0))
         assertEquals("1", attributeValue(loadedDocument, "isLessEqual", "compareValue", 0))
         assertEquals("\n      id = #id#\n      \n    ", textContent(loadedDocument, "isLessEqual", 0))
-        assertFalse(existsAttribute(loadedDocument, "isLessEqual", "prepend", 0))
         assertFalse(existsAttribute(loadedDocument, "isLessEqual", "compareProperty", 0))
 
         assertEquals("or", attributeValue(loadedDocument, "isLessEqual", "prepend", 1))
@@ -32,7 +32,7 @@ internal class IsLessEqualTagConverterTest {
         val ifTagsAfterConvert = convertedDocument.getElementsByTagName("if")
         assertEquals(2, ifTagsAfterConvert.length)
         assertEquals("id <![CDATA[<=]]> 1", attributeValue(convertedDocument, "if", "test", 0))
-        assertEquals("\n       and ( id = #id#\n       ) \n    ", textContent(convertedDocument, "if", 0))
+        assertEquals("\n        and ( id = #id#\n       ) \n    ", textContent(convertedDocument, "if", 0))
         assertFalse(existsAttribute(convertedDocument, "if", "prepend", 0))
         assertFalse(existsAttribute(convertedDocument, "if", "property", 0))
         assertFalse(existsAttribute(convertedDocument, "if", "compareProperty", 0))
@@ -70,7 +70,7 @@ internal class IsLessEqualTagConverterTest {
                 "      testTable\n" +
                 "    WHERE\n" +
                 "      name = #name#\n" +
-                "    <isLessEqual open=\"and (\" property=\"id\" compareValue=\"1\" close=\")\">\n" +
+                "    <isLessEqual prepend=\"and\" open=\"(\" property=\"id\" compareValue=\"1\" close=\")\">\n" +
                 "      id = #id#\n" +
                 "      <isLessEqual prepend=\"or\" property=\"id2\" compareProperty=\"compareId2\">\n" +
                 "        id = #id2#\n" +
