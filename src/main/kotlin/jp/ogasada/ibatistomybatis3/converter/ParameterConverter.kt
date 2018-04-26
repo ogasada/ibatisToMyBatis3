ac8dc43c2ec229be8f0fe3a196e7ef3bc3362a30
ogasada <ogasada0@gmail.com>
2018-04-27 08:56:57 +0900

    feat: add parameter convert when parameterType is primitive in select, insert, update and delete tag

--- src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/ParameterConverter.kt
+++ src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/ParameterConverter.kt
@@ -35,32 +35,76 @@ object ParameterConverter: ITagConverter {
      *
      * ```
      * ${id}
+     *
+     *
+     * ## case 3
+     *
+     * when `parameterType` is primitive or wrapper class in `select`, `insert`, `update` and `delete` tag
+     *
+     * ### before
+     *
+     * ```
+     * <select id="find" parameterType="String" resultType="Map">
+     *     ・・・
+     *     #value#
+     *     $value$
+     *     ・・・
+     * </select>
+     * ```
+     *
+     * ### after
+     *
+     * ```
+     * <select id="find" parameterType="String" resultType="Map">
+     *     ・・・
+     *     #{_parameter}
+     *     ${_parameter}
+     *     ・・・
+     * </select>
      * ```
      */
-    override fun convert(xmlDocument: Document): Document = xmlDocument.convertParameter()
+    override fun convert(xmlDocument: Document): Document = xmlDocument
+            .convertParameter()
+            .convertPrimitiveParameter("select")
+            .convertPrimitiveParameter("insert")
+            .convertPrimitiveParameter("update")
+            .convertPrimitiveParameter("delete")
 
     private fun Document.convertParameter(): Document {
-        fun loop(node: Node) {
-            when (node.nodeType) {
-                Node.ELEMENT_NODE -> {
-                    node as Element
-                    if (node.hasChildNodes()) {
-                        val childNodes = node.childNodes
-                        (0 until childNodes.length).forEach {
-                            loop(childNodes.item(it))
-                        }
-                    }
-                }
-                Node.TEXT_NODE -> {
-                    val textContent = node.textContent
-                    node.textContent = textContent.replace("#([^# ]+?)#".toRegex(), "#{$1}").replace("\\$([^$ ]+?)\\$".toRegex(), "\\\${$1}")
-                }
+        val mapperTags = this.getElementsByTagName("mapper")
+        (0 until mapperTags.length).forEach {
+            convertParameter(mapperTags.item(it) as Element) { it.replace("#([^# ]+?)#".toRegex(), "#{$1}").replace("\\$([^$ ]+?)\\$".toRegex(), "\\\${$1}") }
+        }
+        return this
+    }
+
+    private fun Document.convertPrimitiveParameter(tag: String): Document {
+        val tags = this.getElementsByTagName(tag)
+        (0 until tags.length).forEach {
+            val node = tags.item(it) as Element
+            val parameterType = node.getAttribute("parameterType").toUpperCase()
+            when (parameterType) {
+                "STRING", "INT", "INTEGER", "LONG", "DOUBLE", "FLOAT", "BOOLEAN", "CHAR", "CHARACTER", "SHORT", "BYTE" -> convertParameter(node) { it.replace("#\\{[^}]*}".toRegex(), "#{_parameter}").replace("\\$\\{[^}]*}".toRegex(), "\\\${_parameter}") }
             }
         }
-
-        val mapperTags = this.getElementsByTagName("mapper")
-        (0 until mapperTags.length).forEach { loop(mapperTags.item(it) as Element) }
         return this
+    }
+
+    private fun convertParameter(node: Node, converter: (String) -> String) {
+        when (node.nodeType) {
+            Node.ELEMENT_NODE -> {
+                node as Element
+                if (node.hasChildNodes()) {
+                    val childNodes = node.childNodes
+                    (0 until childNodes.length).forEach {
+                        convertParameter(childNodes.item(it), converter)
+                    }
+                }
+            }
+            Node.TEXT_NODE -> {
+                node.textContent = converter(node.textContent)
+            }
+        }
     }
 }
 
