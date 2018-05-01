dd3e2c3b0a04eb298666aad8a95253818a97dc02
ogasada <ogasada0@gmail.com>
2018-05-01 13:06:28 +0900

    feat: add function when property attribute is not defined
    * use `_parameter` when property attribute is not defined
    * add code for the `test` attribute when parameterClass is `Collection` or `Map`

--- src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/IsNotEmptyTagConverter.kt
+++ src/main/kotlin/jp/ogasada/ibatistomybatis3/converter/IsNotEmptyTagConverter.kt
@@ -7,6 +7,8 @@ object IsNotEmptyTagConverter: ITagConverter {
     /**
      * convert `isNotEmpty` tag as follows
      *
+     * ## case 1
+     *
      * ### before
      *
      * ```
@@ -21,11 +23,29 @@ object IsNotEmptyTagConverter: ITagConverter {
      * ### after
      *
      * ```
-     * <if test="name != null and name != ''">
+     * <if test="!((!_parameter instanceof org.apache.ibatis.session.defaults.DefaultSqlSession$StrictMap and _parameter instanceof java.util.Map and !_parameter.containsKey('name')) or name == null or (name instanceof java.util.Collection and name.size() == 0) or (name.getClass().isArray() and @java.lang.reflect.Array@getLength(name) == 0) or (name instanceof String and name.equals('')))">
      *   and ( name = #name#
-     *   <if test="name2 != null and name2 != ''">
+     *   <if test="!((!_parameter instanceof org.apache.ibatis.session.defaults.DefaultSqlSession$StrictMap and _parameter instanceof java.util.Map and !_parameter.containsKey('name2')) or name2 == null or (name2 instanceof java.util.Collection and name2.size() == 0) or (name2.getClass().isArray() and @java.lang.reflect.Array@getLength(name2) == 0) or (name2 instanceof String and name2.equals('')))">
      *     or name = #name2#
      *   </if> )
+     * </if>
+     * ```
+     *
+     * ## case 2
+     *
+     * ### before
+     *
+     * ```
+     * <isNotEmpty open="and (" close=")">
+     *   name = #name#
+     * </isNotEmpty>
+     * ```
+     *
+     * ### after
+     *
+     * ```
+     * <if test="!((!_parameter instanceof org.apache.ibatis.session.defaults.DefaultSqlSession$StrictMap and _parameter instanceof java.util.Map and !_parameter.containsKey('_parameter')) or _parameter == null or (_parameter instanceof java.util.Collection and _parameter.size() == 0) or (_parameter.getClass().isArray() and @java.lang.reflect.Array@getLength(_parameter) == 0) or (_parameter instanceof String and _parameter.equals('')))">
+     *   and ( name = #name# )
      * </if>
      * ```
      */
@@ -33,9 +53,13 @@ object IsNotEmptyTagConverter: ITagConverter {
             .prependAttributeValueToTextContent("isNotEmpty", "open")
             .prependAttributeValueToTextContent("isNotEmpty", "prepend")
             .appendAttributeValueToTextContent("isNotEmpty", "close")
-                val attributeValue = node.getAttribute("property")
-                "$attributeValue != null and !$attributeValue.isEmpty()"
             .createNewAttribute("isNotEmpty", "test") { _, node ->
+                val attributeValue = if (node.hasAttribute("property")) node.getAttribute("property") else "_parameter"
+                "!((!_parameter instanceof org.apache.ibatis.session.defaults.DefaultSqlSession\$StrictMap and _parameter instanceof java.util.Map and !_parameter.containsKey('$attributeValue')) or " +
+                        "$attributeValue == null or " +
+                        "($attributeValue instanceof java.util.Collection and $attributeValue.size() == 0) or " +
+                        "($attributeValue.getClass().isArray() and @java.lang.reflect.Array@getLength($attributeValue) == 0) or " +
+                        "($attributeValue instanceof String and $attributeValue.equals('')))"
             }
             .removeAttribute("isNotEmpty", "prepend")
             .removeAttribute("isNotEmpty", "open")
